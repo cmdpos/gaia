0e98f93a5f8b459c5d854b4a17c6c31b16304c3d
qzhongwood <2609611@qq.com>
2019-10-17 09:06:35 +0800

    upd

--- shell/git_remote_repo/remote.sh
+++ shell/git_remote_repo/remote.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
+target_branch=release/v2.0.1
+
+git remote remove okcoin
 git remote add okcoin git@gitlab.okcoin-inc.com:dex/gaia.git
 
 # pull okcoin branch info
 git fetch okcoin
 
-# list all remote branch
-git branch -a
-
 # go to official master
-git checkout remotes/offi/master
+git checkout remotes/offi/$target_branch
+
+
+#git branch -D $target_branch
+
+# create a local branch
+git checkout -b $target_branch
 
 # push official master commit history to okcoin master
-git push --set-upstream okcoin master
+git push --set-upstream okcoin $target_branch
