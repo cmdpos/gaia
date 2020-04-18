5faaa092c43d449e7c565fec07f6f9675b819561
zhongqiuwood <zhongqiuwood@gmail.com>
2020-04-18 18:28:00 +0800

    fix export panic bug

--- vendor/github.com/cosmos/cosmos-sdk/x/token/module.go
+++ vendor/github.com/cosmos/cosmos-sdk/x/token/module.go
@@ -46,20 +46,6 @@ func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	return nil
 }
 
-//// default genesis state
-//func (AppModuleBasic) DefaultGenesis() json.RawMessage {
-//	return ModuleCdc.MustMarshalJSON(DefaultGenesisState())
-//}
-//
-//// module validate genesis
-//func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
-//	var data GenesisState
-//	err := ModuleCdc.UnmarshalJSON(bz, &data)
-//	if err != nil {
-//		return err
-//	}
-//	return ValidateGenesis(data)
-//}
 
 // register rest routes
 func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, rtr *mux.Router) {
@@ -135,21 +121,6 @@ func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.Va
 func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	return nil
 }
-
-//// module init-genesis
-//func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
-//	return []abci.ValidatorUpdate{}
-//	var genesisState GenesisState
-//	ModuleCdc.MustUnmarshalJSON(data, &genesisState)
-//	return InitGenesis(ctx, am.keeper, am.accKeeper, am.supplyKeeper, genesisState)
-//}
-//
-//// module export genesis
-//func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
-//	return nil
-//	gs := ExportGenesis(ctx, am.keeper)
-//	return ModuleCdc.MustMarshalJSON(gs)
-//}
 
 // module begin-block
 func (AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
