5a5ed26a365600bdbf8a2128541efea43dd8928a
zhongqiuwood <zhongqiuwood@gmail.com>
2020-04-18 15:41:09 +0800

    evaiocli keys add node${index} -y --home cache/node${index}/evaiocli<<EOF

--- dev/testnet/newvalidator.sh
+++ dev/testnet/newvalidator.sh
@@ -4,13 +4,13 @@
 
 index=$1
 
-gaiacli keys add node${index} -y --home cache/node${index}/gaiacli<<EOF
+evaiocli keys add node${index} -y --home cache/node${index}/evaiocli<<EOF
 12345678
 12345678
 EOF
 
-toAddr=$(gaiacli keys show node${index} -a --home cache/node${index}/gaiacli)
-valPubkey=$(gaiad tendermint show-validator --home cache/node${index}/gaiacli)
+toAddr=$(evaiocli keys show node${index} -a --home cache/node${index}/evaiocli)
+valPubkey=$(gaiad tendermint show-validator --home cache/node${index}/evaiocli)
 
 
 echorun(){
@@ -18,15 +18,15 @@ echorun(){
     $@
 }
 
-echorun gaiacli tx send node0 $toAddr 10000000000neva -b block --yes --chain-id testchain --node tcp://localhost:10057
+echorun evaiocli tx send node0 $toAddr 100000000000neva -b block --yes --chain-id testchain --node tcp://localhost:10057
 
 sleep 10
 
-gaiacli tx staking create-validator --amount 2000000000neva \
+evaiocli tx staking create-validator --amount 10000000000neva \
     --pubkey ${valPubkey} \
     --chain-id testchain --commission-rate 0.1 \
     --commission-max-rate 0.5 \
     --commission-max-change-rate 0.001 \
     -b block \
     --min-self-delegation 1 --from node${index} --moniker node${index} -y --node tcp://localhost:10057 \
-    --home cache/node${index}/gaiacli
+    --home cache/node${index}/evaiocli
