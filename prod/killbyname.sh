8b95a8b0641793f39c61265b78ccf5a7ec4551c2
zhongqiuwood <zhongqiuwood@gmail.com>
2020-06-18 06:26:10 +0800

    add start in prod

--- prod/killbyname.sh
+++ prod/killbyname.sh
@@ -8,6 +8,8 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
+
+
 ps -ef|grep "$NAME"|grep -v grep |grep -v $MYNAME |awk '{print "kill -9 "$2", "$8}'
 ps -ef|grep "$NAME"|grep -v grep |grep -v $MYNAME |awk '{print "kill -9 "$2}' | sh
 echo "All <$NAME> killed!"
